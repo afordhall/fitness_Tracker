8846fc38550913a0c65ae94eb458e7031c1c1902
Athagod <afordhal10@mail.bw.edu>
2020-03-15 22:55:51 -0400

    added models, routes and ran program

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080
+
+
+var mongoose = require("mongoose")
+app.use(express.static("public"))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+var apiRoutes = require("./routes/apiRoutes")
+apiRoutes(app)
+
+var htmlRoutes = require("./routes/htmlRoutes")
+htmlRoutes(app)
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts")
+app.listen(PORT, function(){
+    console.log("App is listening on http://localhost:" + PORT)
+})
