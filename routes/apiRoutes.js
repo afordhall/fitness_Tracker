ad01597dcc6269e561dbd40f1b38b780338c8cae
Athagod <afordhal10@mail.bw.edu>
2020-03-31 19:14:03 -0400

    making sure route work

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,39 +1,39 @@
-// const db = require("../models")
-// function apiRoutes(app) {
-// app.get("/api/workouts", function(req,res){
-//        db.Workout.find({}).then(function(data){
-//            res.json(data)
-//        })
-// })
-// app.get("/api/workouts/range", function(req,res){
-//     db.Workout.find({}).then(function(data){
-//         res.json(data)
-//     })
-// })
+const db = require("../models")
+function apiRoutes(app) {
+app.get("/api/workouts", function(req,res){
+       db.Workout.find({}).then(function(data){
+           res.json(data)
+       })
+})
+app.get("/api/workouts/range", function(req,res){
+    db.Workout.find({}).then(function(data){
+        res.json(data)
+    })
+})
 
-// app.post("/api/workouts", function(req,res){
+app.post("/api/workouts", function(req,res){
   
-//   var newWorkout = req.body
-//     db.Workout.create(newWorkout).then(function(data){
-//         res.json(data)
-//     })
-// })
+  var newWorkout = req.body
+    db.Workout.create(newWorkout).then(function(data){
+        res.json(data)
+    })
+})
 
-// app.put("/api/workouts/:id", function(req,res){
-//     var id = req.params.id
-// db.Workout.findOneAndUpdate(
-//         { _id: id }, 
-//         { $push: { exercises: req.body  } },
-//        function (error, success) {
-//              if (error) {
-//                  console.log(error);
-//              } else {
-//                  console.log(success);
-//                  res.json(success)
-//              }
-//          });
+app.put("/api/workouts/:id", function(req,res){
+    var id = req.params.id
+db.Workout.findOneAndUpdate(
+        { _id: id }, 
+        { $push: { exercises: req.body  } },
+       function (error, success) {
+             if (error) {
+                 console.log(error);
+             } else {
+                 console.log(success);
+                 res.json(success)
+             }
+         });
      
-// })
+})
 
-// }
-// module.exports = apiRoutes
+}
+module.exports = apiRoutes;
