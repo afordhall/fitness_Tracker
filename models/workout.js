a0eb82e4d9b4e0847825373b8aaf6fe1031e03c5
Athagod <afordhal10@mail.bw.edu>
2020-03-31 19:07:46 -0400

    added workouts

--- models/workout.js
+++ models/workout.js
@@ -1,18 +1,18 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 const workoutSchema = new Schema({
-     day: new Date().setDate(new Date().getDate()-10),
-        exercises: [
-          {
-            type: "resistance",
-            name: "Bicep Curl",
-            duration: 20,
-            weight: 100,
-            reps: 10,
-            sets: 4
-          }{
+    //  day: new Date().setDate(new Date().getDate()-10),
+    //     exercises: [
+    //       {
+    //         type: "resistance",
+    //         name: "Bicep Curl",
+    //         duration: 20,
+    //         weight: 100,
+    //         reps: 10,
+    //         sets: 4
+    //       }{
        
-        ]
+    //     ]
       
     day:   { type: Date, default: Date.now},
     exercises:  [
